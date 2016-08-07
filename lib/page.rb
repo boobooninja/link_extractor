b8d24aba73c1ee3458429edba331a407318ce778
Loren Hale <loren.hale@gmail.com>
2016-08-07 14:52:59 -0700

    remove unneeded method in page

--- lib/page.rb
+++ lib/page.rb
@@ -10,6 +10,8 @@ class Page
 
     @match_count = 0
     @matches = []
+
+    @link_regexp = Regexp.new('(([\w+\-]( . )?)+ @ [a-z\d\-]+( \. [a-z]+)* \. [a-z]+|(((http(s)? : \/ \/ )?|[^@ ])([a-zA-Z0-9]+( \. ([a-z]+))+)))')
   end
 
   def add_word(word_line)
@@ -27,23 +29,15 @@ class Page
   end
 
   def parse
-    check_for_links(@parse_string)
-  end
-
-  private
-
-  def link_regexp
-    @link_regexp ||= Regexp.new('(([\w+\-]( . )?)+ @ [a-z\d\-]+( \. [a-z]+)* \. [a-z]+|(((http(s)? : \/ \/ )?|[^@ ])([a-zA-Z0-9]+( \. ([a-z]+))+)))')
-  end
-
-  def check_for_links(parse_string)
-    matches = parse_string.scan(link_regexp)
-    matching_indexes = parse_string.enum_for(:scan, link_regexp).map { Regexp.last_match.begin(0)}
+    matches = @parse_string.scan(@link_regexp)
+    matching_indexes = @parse_string.enum_for(:scan, @link_regexp).map { Regexp.last_match.begin(0)}
 
     matches.each do |match|
       process_match(match, matching_indexes.shift)
     end
   end
+
+  private
 
   def process_match(match, index)
     link_parts = match[0].split(" ")
