a88f1c9605be6113c98dc6835d6c72ed125b611a
Loren Hale <loren.hale@gmail.com>
2016-08-07 10:02:48 -0700

    simplify job

--- lib/job.rb
+++ lib/job.rb
@@ -1,21 +1,15 @@
 class Job
-  attr_reader :page_count
+  attr_accessor :page_count
 
   def initialize
     @page_count = 0
-    @pages = {}
+    @pages = []
     @current_page_number = 0
   end
 
-  def page_count=(num)
-    @page_count = num
-    @page_count.times do |i|
-      @pages[i + 1] = Page.new()
-    end
-  end
-
-  def next_page!
-    @current_page_number += 1 if @current_page_number <= @page_count
+  def new_page
+    @pages << Page.new()
+    @current_page_number = @pages.length - 1
   end
 
   def page
